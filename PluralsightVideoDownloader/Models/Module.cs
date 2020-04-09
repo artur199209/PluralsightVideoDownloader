2ada66917cf88e783600146fcb271ce9445d39b9
asnioszek <artur.snioszek@fenergo.com>
2020-04-09 14:45:00 +0200

    Added models for Module and Clip

---
+++ PluralsightVideoDownloader/Models/Module.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace PluralsightVideoDownloader.Models
+{
+    public class Module
+    {
+        public string Id { get; set; }
+        public string ModuleId { get; set; }
+        public string DeprecatedId { get; set; }
+        public string AuthorId { get; set; }
+        public string Title { get; set; }
+        public string Duration { get; set; }
+        public string PlayerUrl { get; set; }
+        public List<Movie> Clips { get; set; }
+    }
+}
---
+++ PluralsightVideoDownloader/Models/Movie.cs
@@ -0,0 +1,15 @@
+﻿
+namespace PluralsightVideoDownloader.Models
+{
+    public class Movie
+    {
+        public string Id { get; set; }
+        public string ClipId { get; set; }
+        public string DeprecateId { get; set; }
+        public string Title { get; set; }
+        public string Duration { get; set; }
+        public string ModuleTitle { get; set; }
+        public bool Trailer { get; set; }
+        public string PlayerUrl { get; set; }
+    }
+}
