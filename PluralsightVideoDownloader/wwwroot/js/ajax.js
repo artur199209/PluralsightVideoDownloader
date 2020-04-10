5288bb5ef4b4bf4621db29b45b3ac6ec43d1d92d
asnioszek <artur.snioszek@fenergo.com>
2020-04-10 12:56:13 +0200

    Added ajax script

---
+++ PluralsightVideoDownloader/wwwroot/js/ajax.js
@@ -0,0 +1,12 @@
+﻿$(document).ready(function () {
+    $("#downloadCourseBtn").click(function () {
+        $.ajax(
+            {
+                type: "POST",
+                url: "Home/Test",  
+                data: {
+                    Name: $("#courseUrl").val()
+                }
+            });  
+    });
+});
