c7d073ed5372a9bde7ac08bed09881de7d6a0002
asnioszek <artur.snioszek@fenergo.com>
2020-04-10 12:54:57 +0200

    Add helper to get/create directory

---
+++ PluralsightVideoDownloader/Helpers/PathHelper.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+using System.Linq;
+
+namespace PluralsightVideoDownloader.Helpers
+{
+    public static class PathHelper
+    {
+        public static string GetCourseName(string path)
+        {
+            var segments = path.Split('/').ToList();
+            return segments[segments.Count - 2];
+        }
+
+        public static string CombineDownloadPathWithExtension(string mainPath, string courseName, string moduleName, string fileName, string extension)
+        {
+            var pathWithoutExtension = Path.Combine(mainPath, courseName, moduleName);
+
+            if (!Directory.Exists(pathWithoutExtension))
+            {
+                Directory.CreateDirectory(pathWithoutExtension);
+            }
+
+            return Path.Combine(pathWithoutExtension, fileName + extension);
+        }
+    }
+}
