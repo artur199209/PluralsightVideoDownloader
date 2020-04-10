0a7006fa99b7556fb35e4ee3f35cd64716c1cedf
asnioszek <artur.snioszek@fenergo.com>
2020-04-10 12:55:36 +0200

    Updated models

--- PluralsightVideoDownloader/Models/Clip.cs
+++ PluralsightVideoDownloader/Models/Clip.cs
@@ -1,15 +1,12 @@
-﻿
-namespace PluralsightVideoDownloader.Models
+﻿namespace PluralsightVideoDownloader.Models
 {
     public class Clip
     {
         public string Id { get; set; }
         public string ClipId { get; set; }
-        public string DeprecateId { get; set; }
         public string Title { get; set; }
-        public string Duration { get; set; }
         public string ModuleTitle { get; set; }
-        public bool Trailer { get; set; }
-        public string PlayerUrl { get; set; }
+        public string MovieLink { get; set; }
+        public string TranscriptLink { get; set; }
     }
 }
--- PluralsightVideoDownloader/Models/Module.cs
+++ PluralsightVideoDownloader/Models/Module.cs
@@ -5,12 +5,8 @@ namespace PluralsightVideoDownloader.Models
     public class Module
     {
         public string Id { get; set; }
-        public string ModuleId { get; set; }
-        public string DeprecatedId { get; set; }
-        public string AuthorId { get; set; }
+        public string CourseName { get; set; }
         public string Title { get; set; }
-        public string Duration { get; set; }
-        public string PlayerUrl { get; set; }
         public List<Clip> Clips { get; set; }
     }
 }
